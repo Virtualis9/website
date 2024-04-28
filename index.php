377137539a628d8330e291efc880756e271b0aff
Christopher Peacock <122057441+Virtualis9@users.noreply.github.com>
2024-04-28 15:39:22 +0100

    Turned project into Order Sheet

--- index.php
+++ index.php
@@ -4,26 +4,71 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>WWU Order Sheet</title>
 </head>
 
 <body>
-    <form action="index.php" method="post">
-        <label>username:</label><br>
-        <input type="text" name="username"><br>
-        <label>password:</label><br>
-        <input type="password" name="password"><br>
-        <input type="submit" value="Log in">
-    </form>
+    <div>
+        <h1>WWU Order Sheet</h1>
+        <input type="datetime-local">
+        <input type="text" placeholder="DELIVERY ADDRESS">
+    </div>
+    <style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
+    </style>
+    <table style="width:100%">
+        <tr>
+            <td>PE Pipe Coils</td>
+            <td>Part Number</td>
+            <td>Pipe STR</td>
+        </tr>
+        <tr>
+            <td>
+                <input type="checkbox">Serviflex Pipe </option><br>
+                <input type="checkbox">20mm X 100mm </option><br>
+                <input type="checkbox">25mm X 100mm </option><br>
+                <input type="checkbox">25mm X 100mm </option><br>
+                <input type="checkbox">32mm X 50mm </option><br>
+                <input type="checkbox">40mm X 100mm </option><br>
+                <input type="checkbox">55mm X 100mm </option><br>
+                <input type="checkbox">63mm X 50mm </option><br>
+
+            </td>
+            <td>
+                <br>
+                [201270]<br>
+                [201172]<br>
+                [201167]<br>
+                [201167]<br>
+                [201170]<br>
+                [201255]<br>
+                [201184]<br>
+                [202261]<br>
+            </td>
+            <td>
+                <br>
+                STR9<br>
+                SDR11<br>
+                SDR11<br>
+                SDR11<br>
+                SDR12<br>
+                SDR11<br>
+                SDR11<br>
+                SDR11<br>
+            </td>
+        </tr>
+    </table>
+    </div>
 </body>
 
 </html>
 
 
 <?php
-
-echo "{$_POST["username"]}<br>";
-echo "{$_POST["password"]}<br>";
 
 
 ?>
